903f85b7a950f800d750a16629308f61acf07d77
NASAboy342 <sopheakpin342@gmail.com>
2025-09-26 21:47:30 +0700

    add wellcome controller

---
+++ EarthApi/EarthApi/Controllers/WellcomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EarthApi.Controllers
+{
+    public class WellcomeController: ControllerBase
+    {
+        [HttpGet("/")]
+        public string Get() => "Wellcome to Earth API";
+    }
+}
