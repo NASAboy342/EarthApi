4c93b362dc0e7b34699f9805fdd4f222cdc1ad09
PinSopheaktra <sopheakpin342@gmail.com>
2025-11-02 16:19:19 +0700

    add  test

--- EarthApi/EarthApi/Repositories/EarthRepository.cs
+++ EarthApi/EarthApi/Repositories/EarthRepository.cs
@@ -38,6 +38,36 @@ namespace EarthApi.Repositories
                     IconUrl = "https://pincloud.ppiinn.net/files/Earth_Games_Icons_/f17b89a4-36ae-4ec2-9e69-0f9665197c55.png",
                     Route = "/bet-n-run2",
                     Description = "Step into the barnyard and test your luck with BetAndRun fast-paced multiplier game where every step holds a chance to multiply your winnings!"
+                },
+                new GameInfo()
+                {
+                    Id = 1,
+                    Name = "Bet And Run!",
+                    IsEnabled = true,
+                    IsUnderMaintenance = false,
+                    IconUrl = "https://pincloud.ppiinn.net/files/Earth_Games_Icons_/f17b89a4-36ae-4ec2-9e69-0f9665197c55.png",
+                    Route = "/bet-n-run2",
+                    Description = "Step into the barnyard and test your luck with BetAndRun fast-paced multiplier game where every step holds a chance to multiply your winnings!"
+                },
+                new GameInfo()
+                {
+                    Id = 1,
+                    Name = "Bet And Run!",
+                    IsEnabled = true,
+                    IsUnderMaintenance = false,
+                    IconUrl = "https://pincloud.ppiinn.net/files/Earth_Games_Icons_/f17b89a4-36ae-4ec2-9e69-0f9665197c55.png",
+                    Route = "/bet-n-run2",
+                    Description = "Step into the barnyard and test your luck with BetAndRun fast-paced multiplier game where every step holds a chance to multiply your winnings!"
+                },
+                new GameInfo()
+                {
+                    Id = 1,
+                    Name = "Bet And Run!",
+                    IsEnabled = true,
+                    IsUnderMaintenance = false,
+                    IconUrl = "https://pincloud.ppiinn.net/files/Earth_Games_Icons_/f17b89a4-36ae-4ec2-9e69-0f9665197c55.png",
+                    Route = "/bet-n-run2",
+                    Description = "Step into the barnyard and test your luck with BetAndRun fast-paced multiplier game where every step holds a chance to multiply your winnings!"
                 }
                 // new GameInfo()
                 // {
